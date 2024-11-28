a3efd8d0e11cb9cbecdb487961c48f8cf6fca58d
aten039 <l.elvis039@gmail.com>
2024-11-27 23:39:00 -0400

    se crean templates y se crea archivo urls especial para core

--- main/urls.py
+++ main/urls.py
@@ -15,16 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views as core_views
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('home/', core_views.home, name="home"),
-    path('about/', core_views.about, name="about"),
-    path('services/', core_views.services, name="services"),
-    path('store/', core_views.store, name="store"),
-    path('contact/', core_views.contact, name="contact"),
-    path('blog/', core_views.blog, name="blog"),
-    path('sample/', core_views.sample, name="sample"),
+    #urls de core
+    path('', include('core.urls')),
+    #urls de admin
     path('admin/', admin.site.urls),
 ]
