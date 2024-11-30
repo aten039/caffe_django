b3edc11bb47dd83260f28c394bdea0363fed1d6b
aten039 <l.elvis039@gmail.com>
2024-11-30 18:25:49 -0400

    nueva app blog, nuevos modelos para entradas de blug y categorias. se configuro el admin para mostrar mas informacion en dichos modelos.

---
+++ blog/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='nombre', max_length=50)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='creado el')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Actualizado el')
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['-created']
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Titulo')
+    content = models.TextField(verbose_name='Contenido')
+    published = models.DateTimeField(verbose_name='Fecha de publicacion', default=now())
+    image = models.ImageField(verbose_name='Imagen' ,upload_to='blog', null=True, blank=True)
+    author = models.ForeignKey(get_user_model(), verbose_name='Autor', on_delete=models.CASCADE, )
+    categories = models.ManyToManyField(Category, verbose_name='Categorias')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Creado el')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Actualizado el')
+
+    class Meta:
+        verbose_name = 'Entrada'
+        verbose_name_plural = 'Entradas'
+        ordering = ['-created']
+
+    def __str__(self) -> str:
+        return str(self.title)
